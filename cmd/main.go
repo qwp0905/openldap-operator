d8196138a1ccae34f02e3ce03fe3f388a5409892
qwp0905 <qwp0905@gmail.com>
2023-05-14 19:24:50 +0900

    fix webhook generate certs

--- cmd/main.go
+++ cmd/main.go
@@ -35,6 +35,7 @@ import (
 
 	openldapv1 "github.com/qwp0905/openldap-operator/api/v1"
 	"github.com/qwp0905/openldap-operator/internal/controller"
+	"github.com/qwp0905/openldap-operator/pkg/cert"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -55,26 +56,36 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var webhookPort int
+	var subject string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&subject, "subject", "", "The subject for certificate")
+	flag.IntVar(&webhookPort, "webhook-port", 9443, "The webhook port.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
-		Development: true,
+		Development: false,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
+	if err := cert.GenerateIfNotExists(subject); err != nil {
+		setupLog.Error(err, "error on generating certs")
+		os.Exit(1)
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Port:                   webhookPort,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "c38020ad.kwonjin.click",
+		CertDir:                "/tmp/k8s-webhook-server/serving-certs",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
